bb10d7aec544acec5e815be7f9609e05595f9d4f
Dennis Mo <dennismoyunfei@gmail.com>
2023-03-30 12:57:23 -0400

    update script

--- devscripts/test-yourbase.sh
+++ devscripts/test-yourbase.sh
@@ -2,13 +2,13 @@
 
 # Only run in docker container
 # Run initial test with nosetest and yourbase
-echo "\n---------Running initial test------------\n\n"
+echo "---------Running initial test------------"
 /app/devscripts/run_tests.sh
 
 # Edit some files
-echo "\n---------Editing files------------\n\n"
+echo "---------Editing files------------"
 echo "print('Hello World')" >> youtube_dl/extractor/buzzfeed.py
 
 # Run test again
-echo "\n--------Running test again------------\n\n"
+echo "--------Running test again------------"
 /app/devscripts/run_tests.sh
